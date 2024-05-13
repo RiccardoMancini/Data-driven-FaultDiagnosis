8c00df619e9f63d2de941376b07f5fa82f927013
Riccardo Mancini <riccardomancini98@gmail.com>
2024-05-13 14:57:44 +0200

    define createTimetable function

--- main.m
+++ main.m
@@ -1,12 +1,7 @@
-newTable = table();
+addpath("./utils/");
 
-for i = 1:size(table_pin1, 1)
-%for i = 1:1000
-    app = table2array(table_pin1(i, 2:end));
-    %disp(app);
-    tt = array2timetable(app', TimeStep=seconds(0.02));
-    
-    newTable = [newTable; {table_pin1.(1)(i), tt}];
-end
 
-newTable.Properties.VariableNames = {'Fault', 'Serie'};
+table_pin1 = readDataset("./data/data_pin1.csv");
+table_pin1_TT = createTimetable(table_pin1);  
+
+
---
+++ utils/createTimetable.m
@@ -0,0 +1,13 @@
+function newTable = createTimetable(fullTable)
+    newTable = table();
+
+    for i = 1:size(fullTable, 1)
+        app = table2array(fullTable(i, 2:end));
+        tt = array2timetable(app', TimeStep=seconds(0.02));        
+        newTable = [newTable; {fullTable.(1)(i), tt}];
+    end
+
+    newTable.Properties.VariableNames = {'Fault', 'Serie'};
+    
+    clear tt app i;
+end
