92cae3548d625cb91c4c59edb60bd288b1ced736
RiccardoMancini <riccardomancini98@gmail.com>
2024-05-16 00:23:30 +0200

    concat dataTables

--- main.m
+++ main.m
@@ -1,7 +1,37 @@
 addpath("./utils/");
 
+% num_colonne_minimo = Inf; % Inizia con un valore infinito
+% nome_file_minimo = '';
+% 
+% % Ottieni la lista dei file CSV nella directory
+% file_list = dir(fullfile("./data/", '*.csv'));
+% 
+% % Itera sui file CSV
+% for i = 1:length(file_list)
+%     % Leggi il file CSV in una tabella
+%     table_corrente = readtable(fullfile("./data/", file_list(i).name));
+% 
+%     % Ottieni il numero di colonne della tabella corrente
+%     num_colonne = width(table_corrente);
+% 
+%     % Verifica se il numero di colonne è inferiore al minimo attuale
+%     if num_colonne < num_colonne_minimo
+%         num_colonne_minimo = num_colonne;
+%         nome_file_minimo = file_list(i).name;
+%     end
+% end
 
-table_pin1 = readDataset("./data/data_pin1.csv");
-table_pin1_TT = createTimetable(table_pin1);
+
+
+table_pin = readDataBySensorName("pin");
+table_pin_TT = createTimetable(table_pin);
+
+% table_po = readDataBySensorName("po");
+% table_po_TT = createTimetable(table_po);
+
+% table_pdmp = readDataBySensorName("pdmp");
+% table_pdmp_TT = createTimetable(table_pdmp);
+
+
 
 
---
+++ utils/readDataBySensorName.m
@@ -0,0 +1,39 @@
+function fullDataTable = readDataBySensorName(sensore)
+    
+    file_list = dir("./data/");
+    matching_files = [];
+    for i = 1:length(file_list)
+        if contains(file_list(i).name, sensore) && ~file_list(i).isdir
+            matching_files = [matching_files; {file_list(i).name}];
+        end
+    end
+    
+    matching_files = string(matching_files);
+    fullDataTable = [];
+    colonnaEtichetta = 1;
+    max_columns = 673;
+    for i = 1:length(matching_files)
+        disp(strcat("In elaborazione: ", matching_files(i)));
+        dataTable = readtable(strcat("./data/", matching_files(i)), 'ReadVariableNames', false);
+        dataTable = dataTable(: , 1:max_columns);
+        
+        % Converti le colonne in numeri reali (tranne l'etichetta)
+        for j = 1:width(dataTable)
+            if j ~= colonnaEtichetta && iscell(dataTable.(j)) % Controlla se la colonna contiene stringhe
+                dataTable.(j) = str2double(dataTable.(j));
+            end
+        end
+    
+        % Converte la colonna dell'etichetta in interi
+        if iscell(dataTable.(colonnaEtichetta)) % Controlla se la colonna contiene stringhe
+            dataTable.(colonnaEtichetta) = int32(str2double(dataTable.(colonnaEtichetta))); 
+        end
+
+        if isempty(fullDataTable)
+            fullDataTable = dataTable;
+        else
+            % fullDataTable = outerjoin(fullDataTable, dataTable,'MergeKeys', true);
+            fullDataTable = [fullDataTable; dataTable];
+        end
+    end
+end
